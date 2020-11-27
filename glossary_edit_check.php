fbdeaa9ed0990f4b787afd4cc335cfc38e497c6e
ryouym <ryo2003@gmail.com>
2020-11-27 16:27:19 +0900

    Add the escape process to convert it into a string
    The escape process was leaking

--- glossary_edit_check.php
+++ glossary_edit_check.php
@@ -16,6 +16,11 @@ $reading=$_POST['reading']; //前の画面から入力値を受け取り、$read
 $category=$_POST['category']; //前の画面から入力値を受け取り、$categoryに格納
 $description=$_POST['description']; //前の画面から入力値を受け取り、$escriptionに格納
 
+$name=htmlspecialchars($name,ENT_QUOTES,'UTF-8'); //文字列に変換（セキュリティ対策）
+$reading=htmlspecialchars($reading,ENT_QUOTES,'UTF-8'); //文字列に変換（セキュリティ対策）
+$category=htmlspecialchars($category,ENT_QUOTES,'UTF-8'); //文字列に変換（セキュリティ対策）
+$description=htmlspecialchars($description,ENT_QUOTES,'UTF-8'); //文字列に変換（セキュリティ対策）
+
 //$nameがカラならエラーメッセージを表示する
 //$nameが入力されていれば、$nameを表示する
 if($name==''){
