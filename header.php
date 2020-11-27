bef586a3ecef0290ebc56eef55a90f196c89cd04
ryouym <ryo2003@gmail.com>
2020-11-27 17:13:19 +0900

    Separate header file
    Each page had a header data. Since the content was the same, I cut out the header and made it into a single file.

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4452324-17"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-4452324-17');
+  </script>
+  <!-- END-Google Analytics -->
+  <!-- test -->
+
+  <script src="https://www.googleoptimize.com/optimize.js?id=OPT-KL94RLQ"></script> <!-- Google Optimize -->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <?php
+      echo '<meta name="description" content="'.$description.'">';
+      echo "\n"; //\nはソースコードの改行コード。\nはシングルクオテーションで囲うと文字として出力されてしまうので注意
+      echo '<title>'.$title.'</title>';
+      echo "\n"; //\nはソースコードの改行コード。\nはシングルクオテーションで囲うと文字として出力されてしまうので注意
+  ?>
+  <link rel="stylesheet" href="/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p:800" rel="stylesheet">
+</head>
--- index.php
+++ index.php
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4452324-17"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-4452324-17');
-  </script>
-  <!-- END-Google Analytics -->
-  <!-- test -->
+<?php
+$description = "コンサルティング業界固有のカタカナ語や略語、ITやシステム関連の専門用語をまとめた用語集です。";
+$title = "コンサルティング用語集 | 用語一覧";
+require("header.php");
+?>
 
-  <script src="https://www.googleoptimize.com/optimize.js?id=OPT-KL94RLQ"></script> <!-- Google Optimize -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="description" content="コンサルティング用語集">
-  <title>コンサルティング用語集</title>
-  <link rel="stylesheet" href="/main.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p:800" rel="stylesheet">
-
-</head>
 <body>
   <div class="wrapper">
     <div id="site_title"><a href="https://consulting-glossary.com/">コンサル用語集</a></div>
